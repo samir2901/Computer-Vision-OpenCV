f55fd8bf68a2b32c44bb57feb292009b57408c91
samir2901 <thunder.cse2018@gmail.com>
2020-04-25 22:15:55 +0530

    Update

--- WritingImage.py
+++ WritingImage.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 img = cv2.imread("Data/butterfly.jpg")
-img = cv2.imwrite("image.png",img)
+img = cv2.imwrite("Data/image.png",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
